007a02ed066528dcbdb31795b94a98e916b6a764
Nicollas Miyashiro Rezende <32980547+nicollasmiyashiro@users.noreply.github.com>
2022-05-27 16:41:49 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -6,14 +6,5 @@ app = Flask(__name__)
 def pagina_inicial():
     return "Primeiro app no Lab de DEVOPS"
 
-@app.route('/bug')
-def bad():
-    try:
-        raise TypeError()
-    except TypeError as e:
-        print(e)
-    except TypeError as e:
-        print("Duplicado")
-
 if __name__ == '__main__':
     app.run()
